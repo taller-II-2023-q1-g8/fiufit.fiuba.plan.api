e29dc6314ab9e3f6c9baf039346da3b8f1404b78
lautiman <lautiman@gmail.com>
2023-05-04 21:14:57 -0300

    Multimedia Swagger

--- app/Controllers/Http/MultimediasController.ts
+++ app/Controllers/Http/MultimediasController.ts
@@ -2,6 +2,11 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Multimedia from 'App/Models/Multimedia'
 
 export default class MultimediasController {
+  /**
+   * @index
+   * @description Return array of Multimedias
+   * @responseBody 200 - <Multimedia[]>
+   */
   public async index({ response }: HttpContextContract) {
     try {
       const multimedias = await Multimedia.all()
@@ -16,8 +21,13 @@ export default class MultimediasController {
     }
   }
 
-  public async create({}: HttpContextContract) {} // For the frontend
-
+  /**
+   * @store
+   * @description Create Multimedia
+   * @responseBody 200 - <Multimedia>
+   * @responseBody 400 - Multimedia could not be created
+   * @requestBody <Multimedia>
+   */
   public async store({ request, response }: HttpContextContract) {
     try {
       const payload = request.only(['external_id'])
@@ -32,6 +42,12 @@ export default class MultimediasController {
     }
   }
 
+  /**
+   * @show
+   * @description Return Multimedia
+   * @responseBody 200 - <Multimedia>
+   * @responseBody 404 - Multimedia could not be found
+   */
   public async show({ request, response }: HttpContextContract) {
     try {
       const multimedia = await Multimedia.findOrFail(request.param('id'))
@@ -45,10 +61,14 @@ export default class MultimediasController {
     }
   }
 
-  public async edit({}: HttpContextContract) {} // ???
-
   public async update({}: HttpContextContract) {} // ???
 
+  /**
+   * @show
+   * @description Delete Multimedia
+   * @responseBody 200 - DELETED
+   * @responseBody 404 - Multimedia could not be deleted
+   */
   public async destroy({ request, response }: HttpContextContract) {
     try {
       const multimedia = await Multimedia.findOrFail(request.param('id'))
